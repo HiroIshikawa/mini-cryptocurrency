09263d84aa5950f5d1f9cbd8da9a932195cb87ac
Takahiro Ishikawa <hiro52ishikawa@gmail.com>
2018-03-28 23:57:14 -0700

    Move README.md to parent folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
+    var url = 'http://127.0.0.1:4555/';
+    var fetched = ''
+    fetch(url,{mode: 'no-cors'})
+      .then(function(data) {
+          fetched = 'Yes';
+          console.log('Yes');
+      }).catch(function() {
+          fetched = 'No';
+          console.log('No');
+    });
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <p>{fetched}</p>
     );
   }
 }
